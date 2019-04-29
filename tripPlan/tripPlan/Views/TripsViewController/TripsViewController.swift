aa0a5bb2915e5ea7637b99638584c39ad20475bb
Nguyen Tran Cong <Catbui2805@gmail.com>
2019-04-29 15:14:49 +0700

    tableView for trip

---
+++ tripPlan/tripPlan/Views/TripsViewController/TripsViewController.swift
@@ -0,0 +1,46 @@
+//
+//  TripsViewController.swift
+//  tripPlan
+//
+//  Created by Nguyen Tran Cong on 4/29/19.
+//  Copyright © 2019 nguyentran. All rights reserved.
+//
+
+import UIKit
+
+class TripsViewController: UIViewController {
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.dataSource = self
+        
+        TripFuctions.readTrip { [weak self] in
+            // completion
+            self?.tableView.reloadData()
+        }
+    }
+}
+
+// MARK: - UITableViewDelegate, UITableViewDataSource
+extension TripsViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Data.tripModels.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        if cell == nil {
+            cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "cell")
+        }
+        
+        cell!.textLabel?.text = Data.tripModels[indexPath.row].title
+        
+        return cell!
+    }
+    
+    
+}
