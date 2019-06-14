0af57f345526999c2727ef600084439290874885
Nguyen Tran Cong <Catbui2805@gmail.com>
2019-06-14 16:28:46 +0700

    code base

---
+++ News/News/ViewControllers/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  News
+//
+//  Created by Nguyen Tran Cong on 6/14/19.
+//  Copyright © 2019 Nguyen Tran. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
